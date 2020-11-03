44f031dd61cc1e82d5685edd5c4224b9f11defdc
SongTianhe <49253479+SongTianhe@users.noreply.github.com>
2020-11-03 19:31:13 +0100

    Add project files.

---
+++ Losning-pizza-1-core/Models/Pizza.cs
@@ -0,0 +1,12 @@
+﻿namespace Losning_pizza_1_core.Models
+{
+    public class Pizza
+    {
+        public string PizzaType { get; set; }
+        public string Tykkelse { get; set; }
+        public int Antall { get; set; }
+        public string Navn { get; set; }
+        public string Adresse { get; set; }
+        public string Telefonnr { get; set; }
+    }
+}
---
+++ Losning-pizza-1-core/Startup.cs
@@ -0,0 +1,48 @@
+using Losning_pizza_1_core.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Losning_pizza_1_core
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<PizzaContext>(options =>
+                            options.UseSqlite("Data Source=Pizza.db"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
